8a74764de6ac905e99d3b56a8b571bb98b666d6b
Marc Cassuto <marc.cassuto@savoirfairelinux.com>
2013-07-11 11:00:18 -0400

    initial version

---
+++ res_partner_attributes_add_NAS/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2013 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+	'name' : 'Quebec Social Insurance Number (NAS)',
+	'version' : '1.0',
+	"author" : "Savoir-faire Linux (mcassuto@savoirfairelinux.com)",
+	"website" : "http://www.savoirfairelinux.com",
+	'license': 'AGPL-3',
+	'category' : 'Localisation/Canada',
+	'depends' : ['base'],
+	'description': """
+Add the Social Insurance Number (NAS) to the partner form.
+""",
+	'auto_install': False,
+	'installable': True,
+	'data' : ['res_partner_attributes_add_NAS_view.xml']
+}
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
