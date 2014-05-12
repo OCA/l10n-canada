10bd55cd5889d280cecf9c4156dd51d0c666c04f
Marc Cassuto <marc.cassuto@savoirfairelinux.com>
2014-05-12 15:37:28 -0400

    [FIX] fix position of the 3rd table

--- l10n_ca_account_check_writing/__openerp__.py
+++ l10n_ca_account_check_writing/__openerp__.py
@@ -35,6 +35,9 @@ Note that the amount in letter is generated when you enter/change the amount,
 not when you print. You will need to modify it if you change the language of
 the supplier.
 
+To use this module, you will need to install num2words Python library:
+https://pypi.python.org/pypi/num2words
+
 Contributors
 ------------
 * David Cormier (david.cormier@savoirfairelinux.com)
