e863da24162ca44d457fc6b1990140512f7a7c56
Mathieu Benoit <mathieu.benoit@savoirfairelinux.com>
2014-07-18 17:03:59 -0400

    Create amount_in_word at generation of pdf, not use the stocked value.

--- l10n_ca_account_check_writing/report/l10n_ca_check_print.py
+++ l10n_ca_account_check_writing/report/l10n_ca_check_print.py
@@ -21,8 +21,13 @@
 ##############################################################################
 
 import time
+from openerp.tools import config
 from openerp.report import report_sxw
 from openerp.tools.translate import _
+# Odoo's built-in routines for converting numbers to words is pretty bad, especially in French
+# This is why we use the library below. You can get it at:
+# https://pypi.python.org/pypi/num2words
+from num2words import num2words
 
 
 class report_print_check(report_sxw.rml_parse):
@@ -30,6 +35,9 @@ class report_print_check(report_sxw.rml_parse):
 
     def __init__(self, cr, uid, name, context):
         super(report_print_check, self).__init__(cr, uid, name, context)
+        self.cr = cr
+        self.uid = uid
+        self.context = context
         self.number_lines = 0
         self.number_add = 0
         self.extra_message = ''
@@ -37,6 +45,7 @@ class report_print_check(report_sxw.rml_parse):
             'time': time,
             'get_all_lines': self.get_all_lines,
             'get_extra_messages': self.get_extra_messages,
+            'get_amount_in_word': self._get_amount_in_word,
         })
 
     def get_extra_messages(self):
@@ -78,6 +87,104 @@ class report_print_check(report_sxw.rml_parse):
             result = result[:self.MAX_LINES]
 
         return result
+
+    def _get_amount_in_word(self, i_id=None, currency_id=None,
+                            amount=None):
+        cr = self.cr
+        uid = self.uid
+        context = self.context
+        ac_vouch_obj = self.pool.get("account.voucher")
+        if context is None:
+            context = {}
+        if amount is None:
+            amount = ac_vouch_obj.browse(cr, uid, i_id, context=context).amount
+
+        # get lang
+        supplier_lang = context.get('lang', config.get('lang', None))
+        if i_id is not None:
+            partner_id = ac_vouch_obj.browse(cr, uid, i_id,
+                                             context=context).partner_id
+            if partner_id:
+                supplier_lang = partner_id.lang
+
+        context = dict(context, lang=supplier_lang)
+        if currency_id is None:
+            if i_id is None:
+                return None
+            currency_id = self._get_current_currency(cr, uid, i_id,
+                                                     context=context)
+
+        currency = self.pool.get('res.currency').browse(cr, uid,
+                                                        currency_id,
+                                                        context=context)
+        # get the amount_in_word
+        return self._get_amount_line(amount, currency, supplier_lang)
+
+    @staticmethod
+    def _get_amount_line(amount, currency, lang):
+        # max char with font Courier 9.0
+        max_char = 72
+        try:
+            amount_in_word = num2words(int(amount), lang=lang[:2])
+        except NotImplementedError:
+            amount_in_word = num2words(int(amount))
+        currency_name = currency.print_on_check
+        cents = int(amount * 100) % 100
+        AND = custom_translation("and", lang)
+
+        if lang.startswith('fr'):
+            first_line = u'{amount_in_word} {currency_name} {AND} {cents}/100 '
+        else:
+            first_line = u'{amount_in_word} {AND} {cents}/100 {currency_name} '
+
+        first_line = first_line.format(AND=AND, currency_name=currency_name,
+                                       amount_in_word=amount_in_word,
+                                       cents=cents)
+
+        first_line = first_line.title()
+
+        nb_missing_char = max_char - len(first_line)
+        if nb_missing_char:
+            stars = '*' * nb_missing_char
+        else:
+            stars = ''
+        amount_line_fmt = first_line + stars
+        return amount_line_fmt
+
+    def _get_current_currency(self, cr, uid, voucher_id, context=None):
+        """
+        Get the currency of the voucher.
+
+        :param voucher_id: Id of the voucher what i want to obtain current currency.
+        :return: currency id of the voucher
+        :rtype: int
+        """
+        voucher = self.pool.get('account.voucher').browse(cr, uid, voucher_id,
+                                                          context)
+        return voucher.currency_id.id or self._get_company_currency(cr, uid,
+                                                                    voucher.id,
+                                                                    context)
+
+    def _get_company_currency(self, cr, uid, voucher_id, context=None):
+        """
+        Get the currency of the actual company.
+
+        :param voucher_id: Id of the voucher what i want to obtain company currency.
+        :return: currency id of the company of the voucher
+        :rtype: int
+        """
+        ac_vouch_obj = self.pool.get('account.voucher')
+        journal = ac_vouch_obj.browse(cr, uid, voucher_id, context).journal_id
+        return journal.company_id.currency_id.id
+
+
+def custom_translation(s, lang):
+    # Odoo uses the current stack frame, yes, the *stack frame* to determine which language _()
+    # should translate a string in. If we want to translate a string in another language, such as
+    # a supplier's language, we have to resort to hacks such as this one. "context" is sought after
+    # in the stackframe, so we have to set it.
+    context = {'lang': lang}
+    return _(s)
 
 
 class report_print_stub(report_print_check):
