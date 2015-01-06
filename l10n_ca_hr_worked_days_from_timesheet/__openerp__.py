06df1a6234dda06d69d13dd96db277fc9ef5ea8e
dufresnedavid <david.dufresne@savoirfairelinux.com>
2015-01-06 13:08:17 -0500

    Add worked days from timesheet implementation for Canada

---
+++ l10n_ca_hr_worked_days_from_timesheet/__openerp__.py
@@ -0,0 +1,53 @@
+# -*- coding:utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2014 Savoir-faire Linux. All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Canada - Worked Days From Timesheet',
+    'category': 'Localization',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'category': 'Generic Modules/Human Resources',
+    'description': """
+Canada Worked Days From Timesheet
+=================================
+Implementation of the worked days from timesheet function for the Canadian
+Payroll
+
+It imports worked days from the timesheet with activities and hourly rates.
+
+Contributors
+------------
+* David Dufresne <david.dufresne@savoirfairelinux.com>
+* Pierre Lamarche <pierre.lamarche@savoirfairelinux.com>
+""",
+    'author': 'Savoir-faire Linux',
+    'website': 'https://www.savoirfairelinux.com/',
+    'depends': [
+        'hr_worked_days_from_timesheet',
+        'hr_contract_hourly_rate',
+        'hr_activity_on_timesheet',
+        'l10n_ca_hr_payroll',
+    ],
+    'data': [
+    ],
+    'test': [],
+    'demo': [],
+    'installable': True,
+}
