c1e75623f9468dc01cd60aa2e6fc6bb76e596d38
dufresnedavid <david.dufresne@savoirfairelinux.com>
2015-02-16 11:38:05 -0500

    Add functions from OpenUpgrade

--- res_partner_attributes_add_SIN/migrations/7.0.1.2/pre-migration.py
+++ res_partner_attributes_add_SIN/migrations/7.0.1.2/pre-migration.py
@@ -20,15 +20,45 @@
 #
 ##############################################################################
 
+from openerp import release
 import logging
 logger = logging.getLogger('OpenUpgrade')
+
+
+def get_legacy_name(original_name):
+    """
+    Returns a versioned name for legacy tables/columns/etc
+    Use this function instead of some custom name to avoid
+    collisions with future or past legacy tables/columns/etc
+
+    :param original_name: the original name of the column
+    :param version: current version as passed to migrate()
+    """
+    return 'openupgrade_legacy_' + ('_').join(
+        map(str, release.version_info[0:2])) + '_' + original_name
+
+
+def rename_columns(cr, column_spec):
+    """
+    Rename table columns. Typically called in the pre script.
+
+    :param column_spec: a hash with table keys, with lists of tuples as \
+    values. Tuples consist of (old_name, new_name). Use None for new_name \
+    to trigger a conversion of old_name using get_legacy_name()
+    """
+    for table in column_spec.keys():
+        for (old, new) in column_spec[table]:
+            if new is None:
+                new = get_legacy_name(old)
+            logger.info("table %s, column %s: renaming to %s",
+                        table, old, new)
+            cr.execute(
+                'ALTER TABLE "%s" RENAME "%s" TO "%s"' % (table, old, new,))
+            cr.execute('DROP INDEX IF EXISTS "%s_%s_index"' % (table, old))
 
 
 def migrate(cr, version):
     if not version:
         return
 
-    if version < '7.0.1.2':
-        logger.info("table res_partner, column nas: renaming to sin")
-        cr.execute('ALTER TABLE "res_partner" RENAME "nas" TO "sin"')
-        cr.execute('DROP INDEX IF EXISTS "res_partner_nas_index"')
+    rename_columns(cr, {'res_partner': ('nas', 'sin')})
