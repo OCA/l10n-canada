bd90d0ef7af4cb1a47d22b567b5e5fd3e4078a0d
Agathe Mollé <agathe.molle@savoirfairelinux.com>
2015-04-14 14:21:52 -0400

    Port SIN module to v8

--- res_partner_attributes_add_SIN/tests/test_res_partner_sin.py
+++ res_partner_attributes_add_SIN/tests/test_res_partner_sin.py
@@ -1,9 +1,8 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    Odoo, Open Source Management Solution
-#    Copyright (C) 2010 - 2014 Savoir-faire Linux
-#    (<http://www.savoirfairelinux.com>).
+#    Copyright (C) 2013 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -19,7 +18,26 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+from openerp import exceptions
+from openerp.tests import TransactionCase
 
-from . import (
-    res_partner_attributes_add_SIN,
-)
+
+class TestPartnerSin(TransactionCase):
+
+    def setUp(self):
+        super(TestPartnerSin, self).setUp()
+        self.partner = self.env["res.partner"].create({
+            "name": "Test Partner",
+            "is_company": False
+        })
+
+    def test_valid_sin(self):
+        """
+        Test SIN from fake SIN generator
+        :return:
+        """
+        self.partner.sin = 252646757
+
+    def test_invalid_sin(self):
+        with self.assertRaises(exceptions.ValidationError):
+            self.partner.sin = 100000000
